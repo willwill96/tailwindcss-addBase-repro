d187fc91fc3671a573859375f9de7e4b7005f879
William Will <10997562+willwill96@users.noreply.github.com>
2024-06-14 15:37:39 -0600

    push code

---
+++ tailwind-working.config.js
@@ -0,0 +1,16 @@
+const plugin = require('tailwindcss/plugin')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [plugin(({addBase})=>{
+      addBase({
+        '[theme-1], .theme-1': {
+          color: 'red'
+        }
+      })
+    })],
+  }
